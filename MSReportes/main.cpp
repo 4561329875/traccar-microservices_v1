7c52d2b1583add0d2e4d3bcd13901366cdbc77eb
David Toro <fdtoro@espe.edu.ec>
2025-02-19 19:33:13 -0500

    Initial commit

---
+++ MSReportes/main.cpp
@@ -0,0 +1,13 @@
+#include "crow.h"
+
+int main() {
+    crow::SimpleApp app; // Crea la aplicación
+
+    // Define una ruta para manejar solicitudes GET en "/"
+    CROW_ROUTE(app, "/")([]() {
+        return "Hola Mundo desde MSReportes";
+    });
+
+    // Inicia el servidor en el puerto 8080
+    app.port(8080).multithreaded().run();
+}
